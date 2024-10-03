e08fcae2f4ab4543bcc7ca2478eb869f874b7534
Yamiiah <96604349+Yamiiah@users.noreply.github.com>
2024-10-03 09:30:07 +0100

    Add files via upload

---
+++ python code/car_value.py
@@ -0,0 +1,12 @@
+#car value calculator
+#give original car value (fresh off the dealership)
+#factors- year, mileage, any mods, any accidents, imd score?
+#do years as depreciation formula
+
+initial_value = int(input('what is the initial value of the car at 0 years: '))
+years = int(input('how old is your car?: '))
+mileage = int(input('what is the mileage of your cat?: '))
+cat = input("what category is your car in? [A,B,S,N]: ")
+imd_score = float(input('what is your imd score?: '))
+
+
