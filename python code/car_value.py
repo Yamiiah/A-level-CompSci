07beed59138e3c4fea109f2ff82014726722ebe2
Yamiiah <96604349+Yamiiah@users.noreply.github.com>
2024-10-27 19:04:54 +0000

    Update car_value.py

--- python code/car_value.py
+++ python code/car_value.py
@@ -1,12 +1,12 @@
 #car value calculator
-#give original car value (fresh off the dealership)
-#factors- year, mileage, any mods, any accidents, imd score?
-#do years as depreciation formula
 
-initial_value = int(input('what is the initial value of the car at 0 years: '))
-years = int(input('how old is your car?: '))
-mileage = int(input('what is the mileage of your cat?: '))
-cat = input("what category is your car in? [A,B,S,N]: ")
-imd_score = float(input('what is your imd score?: '))
+#initialise my variables
+CarValue = 0
+Mileage = 0
+YearsYouHadCar = 0
 
+#find values for these variables
+CarValue = int(input("what is the initial value of your car?: "))
+Mileage = int(input("what is the mileage of your car?: ")
+YearsYouHadCar = int(input("how long have you had the car in years?: ")
 
