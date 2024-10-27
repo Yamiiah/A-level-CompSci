ee90cf2da986f9362351ea5597b25b49be0aed66
Yamiiah <96604349+Yamiiah@users.noreply.github.com>
2024-10-27 19:22:24 +0000

    Update car_value.py

--- python code/car_value.py
+++ python code/car_value.py
@@ -7,6 +7,12 @@ YearsYouHadCar = 0
 
 #find values for these variables
 CarValue = int(input("what is the initial value of your car?: "))
-Mileage = int(input("what is the mileage of your car?: ")
-YearsYouHadCar = int(input("how long have you had the car in years?: ")
+Mileage = int(input("what is the mileage of your car?: "))
+YearsYouHadCar = int(input("how long have you had the car in years?: "))
+
+#mileage depreciation (i will be using webuyanycar method for calculating such costs)
+if Mileage <= 20000:
+    CarValue = CarValue * 0.8
+elif Mileage > 20000 and Mileage >= 40000:
+    CarValue = CarValue * 0.64
 
