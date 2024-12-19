c31f34ae58ab4accd0a77ff66ac089e5556c8fe7
Yamiiah <96604349+Yamiiah@users.noreply.github.com>
2024-12-19 08:56:49 +0000

    Update car_value.py

--- python code/car_value.py
+++ python code/car_value.py
@@ -38,48 +38,12 @@ else:
 #depreciation by age, this will follow the same code for mileage but with different values
     #values gained from motorway.co.uk
 YearsYouHadCar = int(input("how long have you had the car in years?: "))
-if YearsYouHadCar == 1:
-    CarValue = CarValue * 0.8 #decreases 20% for the first 2 years
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
-elif YearsYouHadCar == 2:
-    CarValue = CarValue * 0.64
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
-elif YearsYouHadCar == 3:
-    CarValue = CarValue * 0.544 #after year 2, decreases by 15%
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
-elif YearsYouHadCar == 4:
-    CarValue = CarValue * 0.4624
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
-elif YearsYouHadCar == 5:
-    CarValue = CarValue * 0.39304
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
-elif YearsYouHadCar == 6:
-    CarValue = CarValue * 0.334084
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
-elif YearsYouHadCar == 7:
-    CarValue = CarValue * 0.2839714
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
-elif YearsYouHadCar == 8:
-    CarValue = CarValue * 0.24137569
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
-elif YearsYouHadCar == 9:
-    CarValue = CarValue * 0.2051693365
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
-elif YearsYouHadCar == 10:
-    CarValue = CarValue * 0.174393936
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
+if YearsYouHadCar == 1 or 2:
+    CarValue = CarValue*(0.8**YearsYouHadCar)
+    print(f"your car is valued at £{CarValue}")
 else:
-    CarValue = CarValue * 0.15
-    CarValue = round(CarValue, 2)
-    print(f"your car value after age depreciation is £{CarValue}")
+    CarV = CarValue*(0.8**YearsYouHadCar)
+    CarV = CarV*(0.85**YearsYouHadCar)
+    print(f"your car is valued at £{CarV}")
+    
 
